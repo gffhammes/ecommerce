672002e3bd3b04dea40290eb0eb75e2925e1c00d
Guilherme Hammes <gffhammes@gmail.com>
2022-08-05 13:07:53 -0300

    refactor: add no-products image to product filter

--- src/components/Products/ProductsList.tsx
+++ src/components/Products/ProductsList.tsx
@@ -1,4 +1,4 @@
-import { Grid } from '@mui/material';
+import { Box, Grid, Stack, Typography } from '@mui/material';
 import React from 'react'
 import { ProductCard } from './ProductCard/ProductCard';
 
@@ -14,7 +14,29 @@ export const ProductsList = ({ products, loading }: IProductsListProps) => {
   }
   
   if (!products || products.length === 0) {
-    return <div>No products to show</div>
+    return (
+      <Stack alignItems='center' spacing={4}>        
+        <Box
+          sx={{
+            height: '30rem',
+            width: '100%',
+            overflow: 'hidden',
+          }}
+        >
+          <Box
+            component='img'
+            src='/images/undraw_taken_re_yn20.svg'
+            alt='Não encontrado'
+            sx={{
+              height: '100%',
+              width: '100%',
+            }}
+          />
+        </Box>
+        <Typography textAlign='center' color='primary.main' fontSize={32}>Nenhum produto encontrado</Typography>
+        <Typography textAlign='center' >Se aplicou algum filtro, tente alterar os valores</Typography>
+      </Stack>
+    )
   }
 
   return (
